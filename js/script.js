220480264ff8c5635beccb797b5aba979bc00846
Pato <plankenau@gmail.com>
2013-12-26 18:46:39 -0600

    fixed verbiage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     var last = new Date(document.lastModified);
     var format = last.getFullYear() + "/" + (last.getMonth()+1) + "/" + last.getDate();
-    $("#lastModified").text(format);
+    $("#lastModified").text("Last modified: "+format);
 });
