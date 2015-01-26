af7cdbd4d21cc0698f161454af1549258794b069
Pato <plankenau@gmail.com>
2015-01-26 14:21:57 -0600

    added basic classes im teaching page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+    // update last modified string
     var last = new Date(document.lastModified);
     var format = last.getFullYear() + "/" + (last.getMonth()+1) + "/" + last.getDate();
     $("#lastModified").text("Last modified: "+format);
+    
+    // hide all classes (in classes page)
+    $(".class").hide();
+
+    $(".class_btn").click(function() {
+      var class_id = $(this).attr("data-class");
+      $("#" + class_id).fadeToggle();
+    });
 });
 
 var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
