919a776ccaf9954f8b1c85e90b0ad56acbef597b
Pato <plankenau@gmail.com>
2015-01-26 14:03:49 -0600

    start of konami code easter egg

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ $(document).ready(function(){
     var format = last.getFullYear() + "/" + (last.getMonth()+1) + "/" + last.getDate();
     $("#lastModified").text("Last modified: "+format);
 });
+
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+$(document).keydown(function(e) {
+  kkeys.push( e.keyCode );
+  if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+    $(document).unbind('keydown',arguments.callee);
+    // do something funny
+    alert("You win.");
+  }
+});
