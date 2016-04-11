b020f514e2fcddad0f3ef45f423d37dccd4fa09a
Pato <plankenau@gmail.com>
2016-04-11 12:07:10 -0500

    made it create a dir if it doesnt exist and store costmaps there

--- files/record.sh
+++ files/record.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 #roslaunch bwi_logging record.launch topics:="/move_base/global_costmap/costmap /move_base/global_costmap/costmap_updates" directory:="~/costmaps" prefix:="costmap" --screen
+mkdir -p ~/costmaps/
+cd ~/costmaps
 rosbag record -ocostmap -j /move_base/global_costmap/costmap /move_base/global_costmap/costmap_updates
